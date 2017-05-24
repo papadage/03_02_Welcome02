ec69e5ea81f86ba54e2cdd386615e68f04cd640c
papadage <gpapadakis@aegis-net.com>
2017-05-24 12:59:20 -0400

    Add project files.

---
+++ Welcome02/Welcome02.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Welcome02
+{
+   class Welcome02
+   {
+      static void Main()
+      {
+         Console.Write("Welcome to C# Programming!");
+         MessageBox.Show("Welcome to C# Programming!");
+      }
+   }
+}
