10679f28525f51c0bdffc68ec984b1084570a616
papadage <gpapadakis@aegis-net.com>
2017-06-16 19:57:10 -0400

    Practice typing commands after not studdying for a while.

--- Welcome02/Welcome02.cs
+++ Welcome02/Welcome02.cs
@@ -1,14 +1,41 @@
-﻿using System;
-using System.Windows.Forms;
+﻿// Figure 3.10: Welcome02.cs
+// Displaying one line of text with two statements
 
-namespace Welcome02
+using System;
+
+class Welcome02
 {
-   class Welcome02
+   static void Main() // Main method begins
    {
-      static void Main()
-      {
-         Console.Write("Welcome to C# Programming!");
-         MessageBox.Show("Welcome to C# Programming!");
-      }
+      Console.Write("Welcome to "); // Writes to console, but no carriage retuirn
+      Console.WriteLine("C# Programming!"); //Writes to console with a carriage return afterwards
    }
 }
+
+
+//using System;
+
+//class Welcome02
+//{
+//   static void Main()
+//   {
+//      Console.Write("Welcome to ");
+//      Console.WriteLine("C# Programming!");
+//   }
+//}
+
+
+//using System;
+//using System.Windows.Forms;
+
+//namespace Welcome02
+//{
+//   class Welcome02
+//   {
+//      static void Main()
+//      {
+//         Console.Write("Welcome to C# Programming!");
+//         MessageBox.Show("Welcome to C# Programming!");
+//      }
+//   }
+//}
